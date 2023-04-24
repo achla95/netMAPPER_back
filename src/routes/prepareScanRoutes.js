521b0fc6e4ee40f910340a6749f20d02648072e7
achla95 <achla95@protonmail.com>
2023-04-24 15:41:33 +0200

    feat: add nmap scan

--- src/routes/prepareScanRoutes.js
+++ src/routes/prepareScanRoutes.js
@@ -1,36 +1,78 @@
 import ScanModel from "../db/models/ScanModel.js"
 import auth from "../middlewares/auth.js"
 import fetchScan from "../middlewares/fetchScan.js"
+import { spawn } from "node:child_process"
+import jwt from "jsonwebtoken"
+
+const nmapScan = (address, options) => {
+  return new Promise((resolve, reject) => {
+    const nmap = spawn("nmap", [address, ...options])
+
+    let stdout = ""
+    nmap.stdout.on("data", (data) => {
+      stdout += data.toString()
+    })
+
+    let stderr = ""
+    nmap.stderr.on("data", (data) => {
+      stderr += data.toString()
+    })
+
+    nmap.on("exit", (code) => {
+      console.log(`Nmap exited with code ${code}`)
+
+      if (code === 0) {
+        resolve(stdout)
+      } else {
+        reject(
+          new Error(`Nmap exited with error code ${code}. stderr: ${stderr}`)
+        )
+      }
+    })
+  })
+}
 
 const prepareScanRoutes = (app) => {
-  // CREATE
   app.post("/scan", auth, async (req, res) => {
-    const { content, command } = req.body
+    const { ipAddress, options, command } = req.body
     const {
       ctx: { session },
     } = req
-    const scan = await new ScanModel({
-      command,
-      content,
-      user: session.user,
-    }).save()
 
-    res.send({ result: scan })
+    try {
+      const result = await nmapScan(ipAddress, options)
+      const scan = await new ScanModel({
+        ipAddress,
+        command,
+        result,
+        user: session.user,
+      }).save()
+
+      res.send({ result: scan })
+    } catch (error) {
+      console.error(error)
+      res.status(500).send(error.message)
+    }
   })
 
-  // READ collection
-  app.get("/scans", async (req, res) => {
-    const scan = await ScanModel.find()
-
-    res.send({ result: scan })
+  app.get("/scans", auth, async (req, res) => {
+    const token = req.headers.authorization
+    const decodedToken = jwt.decode(token)
+    const userId = decodedToken.payload.user.id
+    const scans = await ScanModel.find({
+      "user.id": userId,
+    })
+    const allScans = []
+    scans.forEach((scan) => {
+      allScans.push(scan)
+    })
+    res.send({ result: allScans })
   })
 
-  // READ single
   app.get("/scans/:scanId", fetchScan, async (req, res) => {
-    res.send({ result: req.ctx.post })
+    res.send({ result: req.ctx.scan })
   })
 
-  // DELETE
   app.delete("/scans/:scanId", auth, fetchScan, async (req, res) => {
     const { scan } = req.ctx
 
